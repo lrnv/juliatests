faf02907d2bf9fcb76d5b8028c89424914da8446
Oskar Laverny <laverny@cluster26-math.univ-lyon1.fr>
2020-12-12 16:47:44 +0100

    adding graphs

--- ClaytonParetoLN.jl
+++ ClaytonParetoLN.jl
@@ -1,18 +1,17 @@
 import Random, DatagenCopulaBased, Distributions, ThorinDistributions, Optim, Plots
 import Serialization
 using MultiFloats
-setprecision(256)
+setprecision(512)
 
 
-DoubleType = Float64x2
+DoubleType = Float64x5
 ArbType = BigFloat
 
-Base.rand(::Type{MultiFloat{Float64,2}}) = MultiFloat{Float64,2}(rand(Float64))
-function Base.:^(x::MultiFloat{Float64,2}, y::MultiFloat{Float64,2}) 
-    return MultiFloat{Float64,2}(BigFloat(x)^BigFloat(y))
+Base.rand(::Type{MultiFloat{Float64,5}}) = MultiFloat{Float64,5}(rand(Float64))
+function Base.:^(x::MultiFloat{Float64,5}, y::MultiFloat{Float64,5}) 
+    return MultiFloat{Float64,5}(BigFloat(x)^BigFloat(y))
 end
-Base.round(x::MultiFloat{Float64,2}, y::RoundingMode{:Up}) = round(BigFloat(x),y)
-
+Base.round(x::MultiFloat{Float64,5}, y::RoundingMode{:Up}) = MultiFloat{Float64,5}(round(BigFloat(x),y))
 
 dist_name = "Clayton(7)_Par(1,1)_LN(0,0.83)"
 N = 100000
@@ -25,9 +24,10 @@ n_gammas = 20
 # Simulate the dataset : 
 Random.seed!(123)
 sample = DatagenCopulaBased.simulate_copula(N,DatagenCopulaBased.Clayton_cop(2,7.0))
-marginals = [Distributions.Pareto(1,1), Distributions.LogNormal(0,0.83)]
+marginals = [Distributions.Pareto(2.5,1), Distributions.LogNormal(0,0.83)]
+shifts = [1,0]
 for i in 1:size(sample,2)
-    sample[:,i] = Distributions.quantile.(marginals[i],sample[:,i])
+    sample[:,i] = Distributions.quantile.(marginals[i],sample[:,i]) .- shifts[i]
 end
 sample = DoubleType.(transpose(sample))
 
@@ -72,26 +72,44 @@ par = Optim.minimizer(program2)
 
 # Extracting the solution for a plot
 alpha = par[1:n_gammas] .^2 #make them positives
-rates = reshape(par[(n_gammas+1):3n_gammas],(n_gammas,2)) .^ 2 # make them positives
-rez = hcat(alpha,rates)
+scales = reshape(par[(n_gammas+1):3n_gammas],(n_gammas,2)) .^ 2 # make them positives
+rez = hcat(alpha,scales)
 rez = rez[sortperm(-rez[:,1]),:]
 display(rez)
 
-coefs = ThorinDistributions.get_coefficients(alpha,rates,m)
-x = y = ArbType.(0:0.5:10)
-true_density = (x,y) -> Distributions.pdf(dist,[x,y])
+
+#true_density = (x,y) -> Distributions.pdf(dist,[x,y])
 
 tpl = (2, n_gammas)
-fE = (x,y)->convert(Float64,ThorinDistributions.laguerre_density([convert(ArbType,x), convert(ArbType,y)], E))
-g = (x,y)->convert(Float64,ThorinDistributions.laguerre_density([convert(ArbType,x), convert(ArbType,y)], coefs))
-p1 = Plots.plot(x, y, fE, legend=false, title = "Projection on L_$m", seriestype=:wireframe)
-p2 = Plots.plot(x, y, g, legend=false, title = "Estimation in G_$tpl", seriestype=:wireframe)
-p = Plots.plot(p1,p2, layout = (1,2), size=[1920,1024])
-Plots.display(p)
+# fE = (x,y)->convert(Float64,ThorinDistributions.laguerre_density([convert(ArbType,x), convert(ArbType,y)], E))
+# g = (x,y)->convert(Float64,ThorinDistributions.laguerre_density([convert(ArbType,x), convert(ArbType,y)], coefs))
+
+
+
+# generate some data from the MGC : 
+dist = ThorinDistributions.MultivariateGammaConvolution(BigFloat.(alpha),BigFloat.(scales))
+simu_sample = BigFloat.(deepcopy(sample))
+Random.rand!(dist,simu_sample)
+
+simu_sample = Float64.(simu_sample)
+sample = Float64.(sample)
+N_plot = N
+
+using Plots, StatsPlots, KernelDensity
+p1 = marginalkde(sample[1,1:N_plot],sample[2,1:N_plot];levels=100)
+p2 = marginalkde(simu_sample[1,1:N_plot],simu_sample[2,1:N_plot]; levels=100)
+p = plot(p1,p2,layout=(1,2),size=[1920,1024])
+
+
+
+# p1 = Plots.plot(x, y, fE, legend=false, title = "Projection on L_$m", seriestype=:wireframe)
+# #p2 = Plots.plot(x, y, g, legend=false, title = "Estimation in G_$tpl", seriestype=:wireframe)
+# p = Plots.plot(p1,p2, layout = (1,2), size=[1920,1024])
+# Plots.display(p)
 
 # Save stuff :
 if !isdir(dist_name)
     mkdir(dist_name)
 end
 Plots.savefig(p,"$dist_name/$model_name.pdf")
-Serialization.serialize("$dist_name/$model_name.model",(alpha,rates))
+Serialization.serialize("$dist_name/$model_name.model",(alpha,scales))
